48f929354e716c8d49a312d71dc1ad95722854bf
Jake Partusch <jakepartusch@gmail.com>
2016-11-11 21:46:48 -0600

    adding example config

---
+++ example-config.js
@@ -0,0 +1,10 @@
+const config = {
+    mailgun : {
+        apiKey : 'api-key',
+        domain: 'domain.com'
+    },
+    host: 'localhost',
+    recipientEmail: 'your_email@gmail.com',
+    senderEmail: 'your_email@gmail.com'
+};
+module.exports = config;
